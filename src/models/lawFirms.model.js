2c3352e42884fa45d0f82b90a294f90fe82a963e
AmbujTiwari134 <ambuj.tiwari@unyscape.com>
2021-10-05 22:00:15 +0530

    lawFirm-Registraion-add

--- src/models/lawFirms.model.js
+++ src/models/lawFirms.model.js
@@ -1,90 +1,106 @@
 module.exports = (sequelize, Sequelize) => {
-	return sequelize.define(
-		'lawfirm',
-		{
-			id: {
-				type: Sequelize.UUID,
-				defaultValue: Sequelize.UUIDV4,
-				allowNull: false,
-				primaryKey: true,
-			},
-			en_name: {
-				type: Sequelize.STRING,
-			},
-			ar_name: {
-				type: Sequelize.STRING,
-			},
-			// username: {
-			//   type: Sequelize.STRING,
-			//   allowNull: false,
-			// },
-			// password: {
-			//   type: Sequelize.STRING,
-			// },
-			licenseNumber: {
-				type: Sequelize.STRING,
-			},
-			country: {
-				type: Sequelize.STRING,
-			},
-			language: {
-				type: Sequelize.ARRAY(Sequelize.ENUM({
-					values: ['english', 'arabic'],
-				})),
-				allowNull: false,
-			},
-			logo: {
-				type: Sequelize.STRING,
-			},
-			price: {
-				type: Sequelize.INTEGER,
-			},
-			currency: {
-				type: Sequelize.STRING,
-			},
-			industryExperience: {
-				type: Sequelize.UUID,
-				defaultValue: Sequelize.UUIDV4,
-				allowNull: false,
-			},
-			experience: {
-				type: Sequelize.INTEGER,
-			},
-			numOfLawyer: {
-				type: Sequelize.INTEGER,
-			},
-			jurisdiction: {
-				type: Sequelize.STRING,
-			},
-			legalField: {
-				type: Sequelize.STRING,
-			},
-			service: {
-				type: Sequelize.UUID,
-				defaultValue: Sequelize.UUIDV4,
-				allowNull: false,
-			},
-			expertise: {
-				type: Sequelize.STRING,
-			},
-			rating: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				validate: {
-					min: 1,
-					max: 10,
-				},
-			},
-			isActive: {
-				type: Sequelize.INTEGER,
-			},
-			isDeleted: {
-				defaultValue: 0,
-				type: Sequelize.INTEGER,
-			},
-		},
-		{
-			timestamps: true,
-		},
-	);
+  return sequelize.define(
+    'lawfirm',
+    {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      en_name: {
+        type: Sequelize.STRING,
+      },
+      ar_name: {
+        type: Sequelize.STRING,
+      },
+
+      licenseNumber: {
+        type: Sequelize.STRING,
+      },
+      countryId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+      countryTitle: {
+        type: Sequelize.STRING,
+      },
+      languageId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+      languageTitle: {
+        type: Sequelize.STRING,
+      },
+      logo: {
+        type: Sequelize.STRING,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+      },
+      currency: {
+        type: Sequelize.STRING,
+      },
+      industryId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+
+      industryTitle: {
+        type: Sequelize.STRING,
+      },
+
+      experience: {
+        type: Sequelize.INTEGER,
+      },
+      numOfLawyer: {
+        type: Sequelize.INTEGER,
+      },
+      jurisdiction: {
+        type: Sequelize.STRING,
+      },
+      serviceId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+      seviceTitle: {
+        type: Sequelize.STRING,
+      },
+      subCategoryId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+
+      subCategoryTitle: {
+        type: Sequelize.STRING,
+      },
+
+      expertise: {
+        type: Sequelize.STRING,
+      },
+      rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 10,
+        },
+      },
+      isActive: {
+        type: Sequelize.INTEGER,
+      },
+      isDeleted: {
+        defaultValue: 0,
+        type: Sequelize.INTEGER,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
 };
