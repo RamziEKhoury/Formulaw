0e93ae681120c255029b442e20cb05141f78e794
JitendraUnyscape <jitendra.singh@unyscape.com>
2021-10-06 01:18:55 +0530

    Fixes.

--- src/models/lawFirms.model.js
+++ src/models/lawFirms.model.js
@@ -1,106 +1,106 @@
 module.exports = (sequelize, Sequelize) => {
-  return sequelize.define(
-    'lawfirm',
-    {
-      id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
-      },
-      en_name: {
-        type: Sequelize.STRING,
-      },
-      ar_name: {
-        type: Sequelize.STRING,
-      },
+	return sequelize.define(
+		'lawfirm',
+		{
+			id: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				allowNull: false,
+				primaryKey: true,
+			},
+			en_name: {
+				type: Sequelize.STRING,
+			},
+			ar_name: {
+				type: Sequelize.STRING,
+			},
 
-      licenseNumber: {
-        type: Sequelize.STRING,
-      },
-      countryId: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-      },
-      countryTitle: {
-        type: Sequelize.STRING,
-      },
-      languageId: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-      },
-      languageTitle: {
-        type: Sequelize.STRING,
-      },
-      logo: {
-        type: Sequelize.STRING,
-      },
-      price: {
-        type: Sequelize.INTEGER,
-      },
-      currency: {
-        type: Sequelize.STRING,
-      },
-      industryId: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-      },
+			licenseNumber: {
+				type: Sequelize.STRING,
+			},
+			countryId: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				allowNull: false,
+			},
+			countryTitle: {
+				type: Sequelize.STRING,
+			},
+			languageId: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				allowNull: false,
+			},
+			languageTitle: {
+				type: Sequelize.STRING,
+			},
+			logo: {
+				type: Sequelize.STRING,
+			},
+			price: {
+				type: Sequelize.INTEGER,
+			},
+			currency: {
+				type: Sequelize.STRING,
+			},
+			industryId: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				allowNull: false,
+			},
 
-      industryTitle: {
-        type: Sequelize.STRING,
-      },
+			industryTitle: {
+				type: Sequelize.STRING,
+			},
 
-      experience: {
-        type: Sequelize.INTEGER,
-      },
-      numOfLawyer: {
-        type: Sequelize.INTEGER,
-      },
-      jurisdiction: {
-        type: Sequelize.STRING,
-      },
-      serviceId: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-      },
-      seviceTitle: {
-        type: Sequelize.STRING,
-      },
-      subCategoryId: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-      },
+			experience: {
+				type: Sequelize.INTEGER,
+			},
+			numOfLawyer: {
+				type: Sequelize.INTEGER,
+			},
+			jurisdiction: {
+				type: Sequelize.STRING,
+			},
+			serviceId: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				allowNull: false,
+			},
+			serviceTitle: {
+				type: Sequelize.STRING,
+			},
+			subCategoryId: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				allowNull: false,
+			},
 
-      subCategoryTitle: {
-        type: Sequelize.STRING,
-      },
+			subCategoryTitle: {
+				type: Sequelize.STRING,
+			},
 
-      expertise: {
-        type: Sequelize.STRING,
-      },
-      rating: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        validate: {
-          min: 1,
-          max: 10,
-        },
-      },
-      isActive: {
-        type: Sequelize.INTEGER,
-      },
-      isDeleted: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-    },
-    {
-      timestamps: true,
-    }
-  );
+			expertise: {
+				type: Sequelize.STRING,
+			},
+			rating: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				validate: {
+					min: 1,
+					max: 10,
+				},
+			},
+			isActive: {
+				type: Sequelize.INTEGER,
+			},
+			isDeleted: {
+				defaultValue: 0,
+				type: Sequelize.INTEGER,
+			},
+		},
+		{
+			timestamps: true,
+		},
+	);
 };
