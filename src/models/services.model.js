66dbfa377ede9891580f9c80c2e8e7d7e9cf9a4f
JitendraUnyscape <jitendra.singh@unyscape.com>
2021-10-07 11:41:30 +0530

    Hot fixes.

--- src/models/services.model.js
+++ src/models/services.model.js
@@ -1,36 +1,36 @@
 module.exports = (sequelize, Sequelize) => {
-  return sequelize.define(
-    'service',
-    {
-      id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
-      },
-      en_name: {
-        type: Sequelize.STRING,
-      },
-      ar_name: {
-        type: Sequelize.STRING,
-      },
-      description: {
-        type: Sequelize.STRING,
-      },
-      isBillable: {
-        defaultValue: 0,
-        type: Sequelize.ENUM('0', '1'),
-      },
-      isActive: {
-        type: Sequelize.INTEGER,
-      },
-      isDeleted: {
-        defaultValue: 0,
-        type: Sequelize.INTEGER,
-      },
-    },
-    {
-      timestamps: true,
-    }
-  );
+	return sequelize.define(
+		'servicenew',
+		{
+			id: {
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+				allowNull: false,
+				primaryKey: true,
+			},
+			en_name: {
+				type: Sequelize.STRING,
+			},
+			ar_name: {
+				type: Sequelize.STRING,
+			},
+			description: {
+				type: Sequelize.STRING,
+			},
+			isBillable: {
+				defaultValue: 0,
+				type: Sequelize.INTEGER,
+			},
+			isActive: {
+				type: Sequelize.INTEGER,
+			},
+			isDeleted: {
+				defaultValue: 0,
+				type: Sequelize.INTEGER,
+			},
+		},
+		{
+			timestamps: true,
+		},
+	);
 };
