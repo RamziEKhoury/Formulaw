00c5d11593dda1456dc5ef3f9ca9d0491449d1c4
katiyarsuruchi <suruchi.katiyar@unyscape.com>
2021-11-03 12:04:31 +0530

    community

--- src/Config/db.config.js
+++ src/Config/db.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-	HOST: 'localhost',
-	USER: 'postgres',
-	PASSWORD: 'newpassword',
-	DB: 'formula',
-	dialect: 'postgres',
-	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000,
-	},
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "newpassword",
+
+  DB: "formula",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
