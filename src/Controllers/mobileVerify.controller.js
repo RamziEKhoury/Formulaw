b57e9c20320054650d42f01db0208ec0c98d05fe
JitendraUnyscape <jitendra.singh@unyscape.com>
2021-11-22 20:50:22 +0530

    Fixes.

--- src/Controllers/mobileVerify.controller.js
+++ src/Controllers/mobileVerify.controller.js
@@ -1,19 +1,19 @@
-const client = require('twilio')(
-	process.env.ACCOUNT_SID,
-	process.env.AUTH_TOKEN,
-);
+// const client = require('twilio')(
+// 	process.env.ACCOUNT_SID,
+// 	process.env.AUTH_TOKEN,
+// );
 
 module.exports.getCode = async (req, res) => {
 	try {
-		client.verify
-			.services(process.env.VERIFY_SERVICE_SID)
-			.verifications.create({
-				to: `+${req.query.PhnNumber}`,
-				channel: req.query.channel,
-			})
-			.then((data) => {
-				res.status(200).send(data);
-			});
+		// client.verify
+		// 	// .services(process.env.VERIFY_SERVICE_SID)
+		// 	.verifications.create({
+		// 		to: `+${req.query.PhnNumber}`,
+		// 		channel: req.query.channel,
+		// 	})
+		// 	.then((data) => {
+		// 		res.status(200).send(data);
+		// 	});
 	} catch (error) {
 		res.status(500).send(error);
 	}
@@ -21,15 +21,15 @@ module.exports.getCode = async (req, res) => {
 
 module.exports.verifyCode = async (req, res) => {
 	try {
-		client.verify
-			.services(process.env.VERIFY_SERVICE_SID)
-			.verificationChecks.create({
-				to: `+${req.query.PhnNumber}`,
-				code: req.query.code,
-			})
-			.then((data) => {
-				res.status(200).send(data);
-			});
+		// client.verify
+		// 	.services(process.env.VERIFY_SERVICE_SID)
+		// 	.verificationChecks.create({
+		// 		to: `+${req.query.PhnNumber}`,
+		// 		code: req.query.code,
+		// 	})
+		// 	.then((data) => {
+		// 		res.status(200).send(data);
+		// 	});
 	} catch (error) {
 		res.status(500).send(error);
 	}
