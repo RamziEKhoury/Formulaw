1ca788b39b7697f6977a7664976f19179e2ac7a1
JitendraUnyscape <jitendra.singh@unyscape.com>
2022-01-18 13:24:19 +0530

    Fixes.

--- src/Controllers/stripe.js
+++ src/Controllers/stripe.js
@@ -1,40 +1,43 @@
 const db = require('../models');
 const apiResponses = require('../Components/apiresponse');
-const stripe = require('stripe')('sk_test_51KGKjyIk4lgqI6LMspW5DrqW60FgrgXwwvP8FC6191MuUSHvUku6wtXK0tuP4N3w8vjo8SX5dPCe2WoHaLPXnptb00h4RQIXsT');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
 
-module.exports.createCharge = async (req, res) => {
+module.exports.createCharge = (async (req, res) => {
 	try {
 		let customerIn = null;
 		stripe.customers.create({
-			email: req.body.stripeEmail,
-			source: req.body.stripeToken,
-			name: 'Gourav Hammad',
+			email: req.body.email,
+			source: req.body.id,
+			name: req.body.card.name,
 			address: {
-				line1: 'TC 9/4 Old MES colony',
-				postal_code: '452331',
-				city: 'Indore',
-				state: 'Madhya Pradesh',
-				country: 'India',
+				line1: req.body.card.address_line1,
+				postal_code: req.body.card.address_zip,
+				city: req.body.card.address_city,
+				state: req.body.card.address_state,
+				country: req.body.card.address_country,
 			},
 		})
 			.then((customer) => {
-				console.log('charer--->', customer);
 				customerIn = customer;
 				return stripe.charges.create({
-					amount: 2 * 100, // Charing Rs 25
-					description: 'Web Development Product',
+					amount: parseInt(req.body.appointment.query.cost, 10) * 100, // Charing Rs 25
+					description: req.body.appointment.query.getstarted,
 					currency: 'AED',
 					customer: customer.id,
 				});
 			})
 			.then((charge) => {
+				console.log('charge--->', charge);
 				const data = {
 					customerIn,
 					charge,
 				};
-				res.send('Success').body(data); // If no error occurs
+				return apiResponses.successResponseWithData(res, 'Success', data);
+			})
+			.catch( (e) => {
+				return apiResponses.errorResponse(res, e.raw.message);
 			});
 	} catch (err) {
 		return apiResponses.errorResponse(res, err);
 	}
-};
+});
