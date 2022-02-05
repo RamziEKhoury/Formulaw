df19dac0f751c70dffd662164366d2eaa381a670
JitendraUnyscape <jitendra.singh@unyscape.com>
2022-02-05 19:08:19 +0530

    Hot fixes.

--- src/Controllers/appointment.controller.js
+++ src/Controllers/appointment.controller.js
@@ -218,7 +218,7 @@ module.exports.changeStatus = async (req, res) => {
 						(user.user.firstname ? user.user.firstname : ' ') + ' ' + (user.user.lastname ? user.user.lastname : ' '),
 						user.query.getstarted,
 					);
-					console.log('controller without moment--->', new Date(user.time));
+					console.log('controller without moment--->', new Date(new Date(user.time).toUTCString().slice(0, -3)));
 					console.log('controller time--->', moment(user.time).format('HH:mm A'));
 
 					const device = await User.findOne({where: {id: user.customerId}});
